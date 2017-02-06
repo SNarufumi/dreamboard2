9ce5c52d660aff3cf7a4e41a032b2126952e3df6
代田成史 <narufumishirota@jhon.local>
2017-02-06 22:19:15 +0900

    jsがうまく反映されません。また明日。

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,28 @@
+
+      // 引数で渡した範囲内のランダムな整数を生成するための関数
+      var getRandomInt = function(min, max) {
+           return Math.floor(Math.random() * (max - min + 1)) + min;
+           };  
+   
+      // 要素の取得
+      var $container = $('.container'),
+          $wordList = $('.content').find('li'),
+          containerWidth = $container.width(),
+          containerHeight = $container.height();
+
+      console.log(containerWidth, containerHeight) //960px,500vh
+
+      // liそれぞれをランダムに配置
+      $.map($wordList, function(item, index) {
+        var $item = $(item), // wordListのcontent要素が1番目から順に入ってきます
+        topPos = getRandomInt(0, containerHeight), // 0〜500の間でtopの座標を取得
+        leftPos = getRandomInt(0, containerWidth); // 0〜960の間でleft の座標を取得
+
+
+      // 取得したランダム座標を設定
+      $items.css({
+        top: topPos,
+        left: leftPos
+        });
+      });
+
