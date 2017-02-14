c8e1d804e43c0c015131df106254dc53e54f0579
SNarufumi <my.intern.2015.10.12@gmail.com>
2017-02-14 17:37:18 +0000

    ヘッダーのメニューを追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only:[:new,:edit,:update,:destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :set_category, only: [:show, :edit, :update, :new]
   before_action :set_status, only: [:show, :edit, :update, :new]
